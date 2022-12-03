1427b695c4e5516d959bdac5b09d79401662ea17
Adam Bin Meor Azlan <am03864@surrey.ac.uk>
2022-12-03 03:31:28 +0000

    Added form validation

--- teams/models.py
+++ teams/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 class team(models.Model):
-    name = models.CharField(max_length=100)
-    player_count = models.IntegerField()
-    year_founded = models.IntegerField()
+    name = models.CharField(max_length=100, unique=True)
+    player_count = models.IntegerField(validators = [MinValueValidator(11)])
+    year_founded = models.IntegerField(validators = [MaxValueValidator(2022)])
     owner_email = models.CharField(max_length=100)
 
 
